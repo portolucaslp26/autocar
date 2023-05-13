d53eb17f8ac107befd3fd1494f6725bc082fce2e
Lucas De Lima Porto <llporto@stefanini.com>
2023-05-13 20:24:37 -0300

    Finish filter

--- src/app/lista-carros/lista-carros.component.ts
+++ src/app/lista-carros/lista-carros.component.ts
@@ -19,11 +19,42 @@ export class ListaCarrosComponent implements OnInit {
   disabled = false;
   pageEvent!: PageEvent;
   carrosPaginados: any = [];
-
+  codigo: any;
+  anoDe: any
+  anoAte: any
+  marca: any
+  portas: any
+  status: any
+  modelo: any
+  tipo: any
+  modelos: any[] = [];
   constructor(private paginatorIntl: MatPaginatorIntl) { }
 
   ngOnInit() {
     this.atualizaCarrosPaginados();
+    this.carros.forEach((carro: any) => {
+      this.modelos.push(carro.modelo)
+    })
+    console.log(this.modelos)
+  }
+  filtrarCarros(codigoRef: number, tipo: string, marca: string, modelo: string, status: string, portas: number, anoDe: number, anoAte: number, carros: any[]) {
+    console.log(modelo)
+    console.log(carros)
+
+    this.carrosPaginados =  carros.filter((carro) =>
+      (codigoRef ? carro.codigoRef == codigoRef : true) &&
+      (tipo ? carro.tipo == tipo : true) &&
+      (marca ? carro.marca == marca : true) &&
+      (modelo ? carro.modelo == modelo : true) &&
+      (status ? carro.disponivel : true) &&
+      (portas ? carro.portas == portas : true) &&
+      (anoDe ? carro.ano >= anoDe : true) &&
+      (anoAte ? carro.ano <= anoAte : true)
+    );
+    this.carrosPaginados.forEach((carro: any) => {
+      this.modelos.push(carro.modelo)
+    })
+    this.length = this.carrosPaginados.length;
   }
 
   handlePageEvent(e: PageEvent) {
